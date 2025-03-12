338a8d7a6d01c19e53ea61d238b06532bcd962da
Kevin S. Tran <tanksense@gmail.com>
2025-03-12 18:39:56 +1100

    Week 1

---
+++ Assets/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 6, lookSpeed = 150;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Camera Look
+        Camera.main.transform.Rotate ( -Input.GetAxis("Mouse Y") * lookSpeed * Time.deltaTime,
+                                       0,
+                                       0
+                                     );
+        transform.Rotate ( 0,
+                           Input.GetAxis("Mouse X") * lookSpeed * Time.deltaTime,
+                           0
+                         );
+
+        // WASD movement
+        Vector3 forward = transform.forward * Input.GetAxis("Vertical");
+        Vector3 right   = transform.right   * Input.GetAxis("Horizontal");
+
+        Vector3 moveDir = forward + right;
+        rb.velocity = moveDir * moveSpeed;
+    }
+}
