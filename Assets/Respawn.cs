25ba173e0a52d3a2143c99d74ac5847335a979a1
Kevin S. Tran <tanksense@gmail.com>
2025-04-09 18:39:18 +1000

    Week 5

--- Assets/Respawn.cs
+++ Assets/Respawn.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Respawn : MonoBehaviour
 {
+    public float deathY = 0.05f;
     private Vector3    spawnPos;
     private Quaternion spawnRot;
     private Rigidbody  rb;
@@ -13,5 +14,17 @@ public class Respawn : MonoBehaviour
         spawnPos = transform.position;
         spawnRot = transform.rotation;
         rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (transform.position.y <= deathY) DoRespawn();
+    }
+
+    public void DoRespawn()
+    {
+        transform.position = spawnPos;
+        transform.rotation = spawnRot;
+        rb.velocity = Vector3.zero;
     }
 }
